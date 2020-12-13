dba8691c8161b50ab495f5983dd6b66b9863533c
Balázs Szeidl <szeidl.balazs@hotmail.com>
2020-12-13 17:57:17 +0100

    character class ready, media query final

--- script.js
+++ script.js
@@ -26,18 +26,18 @@ function pageLoaded() {
 	/*Name input létrehozása attribútumokkal */
 		form.insertAdjacentHTML('beforebegin', '<h3 id="playerInput">Select player options!</h3>');
 		form.insertAdjacentHTML('afterbegin', '<div class="formNameContainer"></div>');	
-		let formNameContainer = document.querySelector(".formNameContainer");
+		let formNameContainerVar = document.querySelector(".formNameContainer");
 
-		formNameContainer.insertAdjacentHTML('afterbegin', '<input id="inputName" required autofocus></name>');
-		let inputName = document.querySelector("#inputName");
-		inputName.setAttribute("id", "inputName");
-		inputName.setAttribute("type", "text");
-		inputName.setAttribute("placeholder", "Name");
+		formNameContainerVar.insertAdjacentHTML('afterbegin', '<input id="inputName" required autofocus></name>');
+		let inputNameVar = document.querySelector("#inputName");
+		inputNameVar.setAttribute("id", "inputName");
+		inputNameVar.setAttribute("type", "text");
+		inputNameVar.setAttribute("placeholder", "Name");
 
 	/*select gender létrehozása option-ökkel */
 		form.insertAdjacentHTML('beforeend', '<div class="formGenderContainer"></div>');
-		let formGenderContainer = document.querySelector(".formGenderContainer");
-		formGenderContainer.insertAdjacentHTML('afterbegin', '<select id="selectGender"></select>');
+		let formGenderContainerVar = document.querySelector(".formGenderContainer");
+		formGenderContainerVar.insertAdjacentHTML('afterbegin', '<select id="selectGender"></select>');
 		let selectGenderVar = document.querySelector("#selectGender");
 		selectGenderVar.insertAdjacentHTML('afterbegin', '<option id="blank_option" value="" disabled selected hidden>Choose gender!</option>');
 		selectGenderVar.insertAdjacentHTML('afterbegin', '<option id="male" class="optionClass">male</option>');
@@ -47,10 +47,11 @@ function pageLoaded() {
 
 	/*select race létrehozása option-ökkel */
 		form.insertAdjacentHTML('beforeend', '<div class="formRaceContainer"></div>');
-		let formRaceContainer = document.querySelector(".formRaceContainer");
-		formRaceContainer.insertAdjacentHTML('afterbegin', '<select id="selectRace"></select>');
+		let formRaceContainerVar = document.querySelector(".formRaceContainer");
+		formRaceContainerVar.insertAdjacentHTML('afterbegin', '<select id="selectRace"></select>');
 		let selectRaceVar = document.querySelector("#selectRace");
 		selectRaceVar.insertAdjacentHTML('afterbegin', '<option class="blankSelect" value="" disabled selected hidden>Select race!</option>');
+		selectRaceVar.insertAdjacentHTML('afterbegin', '<option class="blankSelect" value=""></option>');
 		selectRaceVar.insertAdjacentHTML('beforeend', '<option id="aarakocra">Aarakocra</option>');
 		selectRaceVar.insertAdjacentHTML('beforeend', '<option id="aasimar">Aasimar</option>');
 		selectRaceVar.insertAdjacentHTML('beforeend', '<option id="centaur">Centaur</option>');
@@ -61,58 +62,58 @@ function pageLoaded() {
 		form.insertAdjacentHTML('beforeend', '<div class="formClassContainer"></div>');
 		let formClassContainer = document.querySelector(".formClassContainer");
 		formClassContainer.insertAdjacentHTML('afterbegin', '<select id="selectClass"></select>');
-		let selectClass = document.querySelector("#selectClass");
-		selectClass.insertAdjacentHTML('afterbegin', '<option class="blankSelect" value="" disabled selected hidden>Select class!</option>');
-		selectClass.insertAdjacentHTML('beforeend', '<option id="artificer">Artificer</option>');
-		selectClass.insertAdjacentHTML('beforeend', '<option id="barbarian">Barbarian</option>');
-		selectClass.insertAdjacentHTML('beforeend', '<option id="cleric">Cleric</option>');
-		selectClass.insertAdjacentHTML('beforeend', '<option id="druid">Druid</option>');
-		selectClass.insertAdjacentHTML('beforeend', '<option id="monk">Monk</option>');
+		let selectClassVar = document.querySelector("#selectClass");
+		selectClassVar.insertAdjacentHTML('afterbegin', '<option class="blankSelect" value="" disabled selected hidden>Select class!</option>');
+		selectClassVar.insertAdjacentHTML('beforeend', '<option id="artificer">Artificer</option>');
+		selectClassVar.insertAdjacentHTML('beforeend', '<option id="barbarian">Barbarian</option>');
+		selectClassVar.insertAdjacentHTML('beforeend', '<option id="cleric">Cleric</option>');
+		selectClassVar.insertAdjacentHTML('beforeend', '<option id="druid">Druid</option>');
+		selectClassVar.insertAdjacentHTML('beforeend', '<option id="monk">Monk</option>');
 
 		/*A character sheet-et befoglaló container létrehozása */	
 		formContainer.insertAdjacentHTML('afterend', '<div class="characterSheetContainer"></div');
-		let characterSheetContainer = document.querySelector(".characterSheetContainer");
+		let characterSheetContainerVar = document.querySelector(".characterSheetContainer");
 	
 	/*character name blokk létrehozása */	
-		characterSheetContainer.insertAdjacentHTML('afterbegin', '<div class="characterNameContainer"></div>');
+		characterSheetContainerVar.insertAdjacentHTML('afterbegin', '<div class="characterNameContainer"></div>');
 		let characterNameContainer = document.querySelector(".characterNameContainer");
 		characterNameContainer.insertAdjacentHTML('beforebegin', '<h3 id="playerData">Player data</h3>');
 		characterNameContainer.insertAdjacentHTML('afterbegin', '<p id="characterName"></p>');
 		let characterName = document.querySelector("#characterName");
 
 	/*character gender blokk létrehozása */
-		characterSheetContainer.insertAdjacentHTML('beforeend', '<div class="characterGenderContainer"></div>');
-		let characterGenderContainer = document.querySelector(".characterGenderContainer");
-		characterGenderContainer.insertAdjacentHTML('afterbegin', '<p id="characterGender"></p>');
+		characterSheetContainerVar.insertAdjacentHTML('beforeend', '<div class="characterGenderContainer"></div>');
+		let characterGenderContainerVar = document.querySelector(".characterGenderContainer");
+		characterGenderContainerVar.insertAdjacentHTML('afterbegin', '<p id="characterGender"></p>');
 		let characterGender = document.querySelector("#characterGender");
-		characterGenderContainer.insertAdjacentHTML('beforeend', '<div class="characterImageContainer"></div>');		
-		let characterImageContainer = document.querySelector(".characterImageContainer");
+		
 		
 
 	/*character race blokk létrehozása */
-		characterSheetContainer.insertAdjacentHTML('beforeend', '<div class="characterRaceContainer"></div>');
-		let characterRaceContainer = document.querySelector(".characterRaceContainer");
-		characterRaceContainer.insertAdjacentHTML('afterbegin', '<p id="characterRace"></p>');
-		let characterRace = document.querySelector("#characterRace");
-		characterRaceContainer.insertAdjacentHTML('beforeend', '<div class="characterImageContainer"></div>');
-		let charImgContVar = document.querySelector('.characterImageContainer');
+		characterGenderContainerVar.insertAdjacentHTML('afterend', '<div class="characterRaceContainer"></div>');
+		let characterRaceContainerVar = document.querySelector(".characterRaceContainer");
+		characterRaceContainerVar.insertAdjacentHTML('afterbegin', '<p id="characterRace"></p>');
+		let characterRaceVar = document.querySelector("#characterRace");
+		characterRaceContainerVar.insertAdjacentHTML('beforeend', '<div class="charRaceImgCont"></div>');
+		let charRaceImgContVar = document.querySelector(".charRaceImgCont");
 
 	/*character class blokk létrehozása */
-		characterSheetContainer.insertAdjacentHTML('beforeend', '<div class="characterClassContainer"></div>');
-		let characterClassContainer = document.querySelector(".characterClassContainer");
-		characterClassContainer.insertAdjacentHTML('afterbegin', '<p id="characterClass"></p>');
-		characterClassContainer.insertAdjacentHTML('beforeend', '<div class="characterImageContainer"></div>');
-		let characterClass = document.querySelector("#characterClass");
+		characterSheetContainerVar.insertAdjacentHTML('beforeend', '<div class="characterClassContainer"></div>');
+		let charClassContVar = document.querySelector(".characterClassContainer");
+		charClassContVar.insertAdjacentHTML('afterbegin', '<p id="characterClass"></p>');
+		let charClassVar = document.querySelector("#characterClass");
+		charClassContVar.insertAdjacentHTML('beforeend', '<div class="charClassImgCont"></div>');
+		let charClassImgContVar = document.querySelector(".charClassImgCont");
+	
 
-	/*ikonok és image-ek */
-		
+	
 	
 	
 		/*name input - output kapcsolatok */	
 	document.getElementById("inputName").addEventListener("change", nameOutput);
 	function nameOutput() {
-		console.log(inputName.value);
-		characterName.innerHTML = "Name: " + inputName.value;
+		console.log(inputNameVar.value);
+		characterName.innerHTML = "Name: " + inputNameVar.value;
 	}
 
 	/*gender input - output kapcsolatok */
@@ -130,7 +131,7 @@ function pageLoaded() {
 			characterGender.innerHTML = "Gender: " + selectGenderVar.value + " " + '<img src="img/iconmonstrgender10.svg">';
 		}
 		else {
-			console.log("no gender selected");
+			console.log("No gender selected!");
 		}		
 	}
 
@@ -139,34 +140,35 @@ function pageLoaded() {
 	document.getElementById("selectRace").addEventListener("change", raceOutput);
 	function raceOutput() {
 		console.log(selectRaceVar.value);
+		 		
 		if (selectRaceVar.value === "Aarakocra") {
-			characterRace.innerHTML = "Race: " + selectRaceVar.value;
-			charImgContVar.innerHTML = '<img src="img/Aarakocra.png">';
+			characterRaceVar.innerHTML = "Race: " + selectRaceVar.value;
+			charRaceImgContVar.innerHTML = '<img src="img/Aarakocra.png">';
 			
 		}
 		
 		else if(selectRaceVar.value === "Aasimar") {
-			characterRace.innerHTML = "Race: " + selectRaceVar.value;
-			charImgContVar.innerHTML = '<img src="img/Aasimar.png">';
+			characterRaceVar.innerHTML = "Race: " + selectRaceVar.value;
+			charRaceImgContVar.innerHTML = '<img src="img/Aasimar.png">';
 		}
 
 		else if(selectRaceVar.value === "Dragonborn") {
-			characterRace.innerHTML = "Race: " + selectRaceVar.value;
-			charImgContVar.innerHTML = '<img src="img/Dragonborn.png">';
+			characterRaceVar.innerHTML = "Race: " + selectRaceVar.value;
+			charRaceImgContVar.innerHTML = '<img src="img/Dragonborn.png">';
 		}
 
 		else if(selectRaceVar.value === "Dwarf") {
-			characterRace.innerHTML = "Race: " + selectRaceVar.value;
-			charImgContVar.innerHTML = '<img src="img/Dwarf.png">';
+			characterRaceVar.innerHTML = "Race: " + selectRaceVar.value;
+			charRaceImgContVar.innerHTML = '<img src="img/Dwarf.png">';
 		}
 
 		else if(selectRaceVar.value === "Centaur") {
-			characterRace.innerHTML = "Race: " + selectRaceVar.value;
-			charImgContVar.innerHTML = '<img src="img/Centaur.png">';
+			characterRaceVar.innerHTML = "Race: " + selectRaceVar.value;
+			charRaceImgContVar.innerHTML = '<img src="img/Centaur.png">';
 		}
 
 		else {
-			characterRace.innerHTML = "";
+			characterRaceVar.innerHTML = "No race selected!";
 		}
 
 	}
@@ -174,8 +176,38 @@ function pageLoaded() {
 	/*class input - output kapcsolatok */
 	document.getElementById("selectClass").addEventListener("change", classOutput);
 	function classOutput() {
-		console.log(selectClass.value);
-		characterClass.innerHTML = "Class: " + selectClass.value;
+		console.log(selectClassVar.value);
+		
+		if (selectClassVar.value === 'Artificer') {
+			charClassVar.innerHTML = "Class: " + selectClassVar.value;
+			charClassImgContVar.innerHTML = '<img src="img/artificer.png">';
+		}
+
+		else if (selectClassVar.value === 'Barbarian') {
+			charClassVar.innerHTML = "Class: " + selectClassVar.value;
+			charClassImgContVar.innerHTML = '<img src="img/barbarian.png">';
+		}
+
+		else if (selectClassVar.value === 'Cleric') {
+			charClassVar.innerHTML = "Class: " + selectClassVar.value;
+			charClassImgContVar.innerHTML = '<img src="img/cleric2.png">';
+		}
+
+		else if (selectClassVar.value === 'Druid') {
+			charClassVar.innerHTML = "Class: " + selectClassVar.value;
+			charClassImgContVar.innerHTML = '<img src="img/druid2.png">';
+		}
+
+		else if (selectClassVar.value === 'Monk') {
+			charClassVar.innerHTML = "Class: " + selectClassVar.value;
+			charClassImgContVar.innerHTML = '<img src="img/monk2.png">';
+		}
+
+		else {
+			charClassVar.innerHTML = "No class selected! ";
+			charClassImgContVar.innerHTML = "";
+		}
+		
 	}
 	
 };
